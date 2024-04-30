1a27820b67e64000b1bca960fea929306e4aa97d
amha-mersha <amhamersh@gmail.com>
2024-04-30 23:48:09 +0300

    added color mode switch

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,17 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+
+const ColorModeSwitch = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+  return (
+    <HStack>
+      <Switch
+        padding={"10px"}
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      />
+      <Text>DarkMode</Text>
+    </HStack>
+  );
+};
+
+export default ColorModeSwitch;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import { HStack, Image, Text } from "@chakra-ui/react"
 import logo from "../assets/logo_white.png"
+import ColorModeSwitch from "./ColorModeSwitch"
 
 const NavBar = () => {
   return (
     <HStack>
         <Image src={logo} w={"3%"} />
-        <Text> NavBar</Text>
+        <ColorModeSwitch />
 
     </HStack>
 )
