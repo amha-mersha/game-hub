665a25c338d42f362faa3513c775dd90849a6b07
amha-mersha <amhamersh@gmail.com>
2024-05-01 10:20:29 +0300

    displaying icon for platforms

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,42 @@
+import {
+  FaWindows,
+  FaLinux,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platforms } from "../hooks/useGame";
+import { IconType } from "react-icons";
+import { HStack, Icon } from "@chakra-ui/react";
+
+interface Props {
+  platforms: Platforms[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    ios: MdPhoneIphone,
+    android: FaAndroid,
+    mac: FaApple,
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    nintendo: SiNintendo,
+    xbox: FaXbox,
+    linux: FaLinux,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform ) => (
+        <Icon as={iconMap[platform.slug]} color="gray.400" />
+    ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -6,6 +6,13 @@ export interface Game {
   id: number;
   name: string;
   background_image: string
+  parent_platforms: {platform: Platforms}[]
+}
+
+export interface Platforms{
+  id: number,
+  name: string, 
+  slug: string,
 }
 
 export interface FetchGamesResponse {
